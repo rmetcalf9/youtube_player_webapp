0331f448c3a5d36e47ae78667c7cf557d3b56c09
Robert <rmetcalf9@googlemail.com>
2023-10-20 09:29:15 +0100

    Initial version

---
+++ quasar-project/src/components/MyVideoPlayer.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="wholePage">
+    <VideoPlayer
+        class="wholePage"
+        ref="videoPlayer"
+        :options="playerOptions"
+        @play="onPlay"
+        @pause="onPause"
+        @ended="onEnded"
+    />
+  </div>
+</template>
+
+<script>
+// npm install video.js @videojs-player/vue videojs-youtube --save
+// https://github.com/videojs/videojs-youtube
+// https://docs.videojs.com/
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+import 'videojs-youtube/dist/Youtube.min.js'
+
+export default {
+  name: 'MyVideoPlayer',
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  components: {
+    VideoPlayer
+  },
+  data() {
+    return {
+      player: null,
+      playerOptions: {
+        autoplay: false,
+        controls: true,
+        techOrder: ['youtube'],
+        sources: [
+          {
+            type: 'video/youtube',
+            src: 'https://www.youtube.com/watch?v=W3s2YdgaS5E',
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+  },
+  beforeUnmount() {
+  },
+  methods: {
+    onEnded() {
+      console.log('onEnded')
+    },
+    onPlay() {
+      console.log('onPlay')
+    },
+    onPause() {
+      console.log('onPause')
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wholePage {
+  width: 100%;
+  max-width: 1200px;
+  min-height: inherit;
+  margin: auto;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+</style>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page>
+    <MyVideoPlayer
+    />
+    <q-btn
+      @click="onPressBtn"
+      color="primary"
+      label="Test Button"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import MyVideoPlayer from '../components/MyVideoPlayer.vue'
+
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    MyVideoPlayer
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    onPressBtn () {
+      console.log('onpressbtn')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wholePage {
+  width: 100%;
+  max-width: 1200px;
+  min-height: inherit;
+  margin: auto;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+</style>
